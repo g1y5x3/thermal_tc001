f6cb4aa22806207b29b06427ce812eb5408db022
Yixiang <ygao@mst.edu>
2026-07-24 15:32:20 -0500

    update thermal camera driver for better exit during launch

--- thermal_tc001/thermal_node.py
+++ thermal_tc001/thermal_node.py
@@ -141,7 +141,8 @@ def main(args=None):
         if node is not None:
             # Ensure proper cleanup
             node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
